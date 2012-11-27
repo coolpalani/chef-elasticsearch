c4cf10ca39da15bdbcc2f750ef3ee9e00a28f201
Peter Donald <peter@realityforge.org>
2012-11-27 11:22:22 +1100

    Setup basic cookbook infrastructure

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'elasticsearch'
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Installs and configures elasticsearch'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.5.0'
+
+depends 'java'
+recommends 'authbind'
